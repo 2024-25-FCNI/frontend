c65149cde0861e110a8ee57d746cfdd39fa1bc53
nedairisz <iriszneda@gmail.com>
2025-02-04 09:26:11 +0100

    reg. bej. szem

--- src/pages/Bejelentkezes.js
+++ src/pages/Bejelentkezes.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import useAuthContext from "../contexts/AuthContext";
 
-
 export default function Bejelentkezes() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
