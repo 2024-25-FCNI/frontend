8b77577be263e0176abc5a72229f51f7d7676f22
nedairisz <iriszneda@gmail.com>
2025-05-04 16:49:00 +0200

    pontos idő

--- src/components/admin/TermekAdmin.js
+++ src/components/admin/TermekAdmin.js
@@ -12,10 +12,22 @@ export function TermekAdmin({ termek }) {
     }
   };
 
-  const formatDate = (dateString) => {
+  /*  const formatDate = (dateString) => {
     if (!dateString) return "";
     const date = new Date(dateString);
     return date.toISOString().slice(0, 16).replace("T", " ");
+  }; */
+
+  const formatDate = (dateString) => {
+    if (!dateString) return "";
+    const date = new Date(dateString);
+    return date.toLocaleString("hu-HU", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
   };
 
   return (
@@ -25,7 +37,7 @@ export function TermekAdmin({ termek }) {
     >
       <td>{termek.cim}</td>
       <td>{termek.bemutatas}</td>
-      
+
       <td className="admincell">{termek.leiras}</td>
 
       <td>{termek.ar} Ft</td>
